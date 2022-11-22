d796ad3e2848366d725bbb4500d1241a205ff4cd
the15thsin <ayushjalan1203@gmail.com>
2022-11-22 19:05:24 +0530

    updated

--- assignment.c
+++ assignment.c
@@ -18,23 +18,31 @@ struct Node{
         struct Node *next;
 };
 
-// struct Node* create(){
-    
-//     return head;
-// }
 
+/* ========================================================================================
+                                Printing of a linked list
+===========================================================================================*/
 void displayLL(struct Node *ptr){
-        system("cls");
-    while(ptr!=NULL)
-    {
-        printf("%d -> ",ptr->data);
-        ptr = ptr->next;
+    system("clear");
+    printf("\n=========================================================\n");
+    if(ptr==NULL){
+        printf("\t\t(empty)");
     }
-    printf("\nEnter any key and enter to continue...");
-    char ch;
-    scanf("%c",&ch);
+    else{
+        while(ptr!=NULL)
+        {
+            printf("%d -> ",ptr->data);
+            ptr = ptr->next;
+        }
+    }
 }
+//============================================================================================
 
+
+
+/* ========================================================================================
+                                Insertion of a node
+===========================================================================================*/
 struct Node* insertBeg(struct Node *head){
     struct Node *elem;
     elem = (struct Node*) malloc (sizeof(struct Node));
@@ -45,95 +53,169 @@ struct Node* insertBeg(struct Node *head){
     return elem;
 }
 
-int insertEnd(){
-    return 2;
+void insertEnd(struct Node *ptr){
+    while(ptr->next!=NULL){
+        ptr = ptr->next;
+    }
+    struct Node *elem;
+    elem = (struct Node*)malloc(sizeof(struct Node));
+    printf("\nEnter the value to insert at the end : ");
+    scanf("%d",&(elem->data));
+    ptr->next = elem;
+    elem->next = NULL;
 }
 
-int insertN(){
-    return 3;
+void insertn(struct Node *ptr){
+    printf("Enter the index(starts from 1): ");
+    int i;
+    scanf("%d",&i);
+    i=i-1;
+    while(--i)
+    {
+        ptr = ptr->next;
+    }
+    struct Node *elem;
+    elem = (struct Node*)malloc(sizeof(struct Node)); 
+    printf("Enter the value to insert : ",i);
+    scanf("%d",&(elem->data));
+    elem->next = ptr->next;
+    ptr->next = elem;
 }
 
 struct Node* insert(struct Node *head){
-    system("cls");
-    printf("\t===========Insertion of a Node==========\n\n");
+    system("clear");
+    printf("\n===========Insertion of a Node==========\n\n");
     printf("Where you want to insert a node? \n");
-    printf("\t1. At beginning\n");
-    printf("\t2. In the end\n");
-    printf("\t3. At a position 'n'\n");
-    printf("\t4. Main Menu\n");
-    printf("\n\t Enter your choice : ");
+    printf("1. At beginning\n");
+    printf("2. In the end\n");
+    printf("3. At 'nth' index\n");
+    printf("4. Main Menu\n");
+    printf("Enter your choice : ");
     int ch;
     scanf("%d", &ch);
     switch(ch){
         case 1: head = insertBeg(head);
                 break;
                 
-        case 2: insertEnd();
+        case 2: insertEnd(head);
                 break;
                 
-        case 3: insertN();
+        case 3: insertn(head);
                 break;
                 
         case 4: exit;
                 break;
         
         default: printf("Wrong choice entered!!!\n\n");
-                system("cls");
+                system("clear");
                 exit;
     }
     return head;
 }
 
-main(){
+//=============================================================================================
+
+
+
+
+/* ========================================================================================
+                                Deletion of a node
+===========================================================================================*/
+
+struct Node* deleteByValue(struct Node *ptr){
+    printf("Enter the value to delete : ");
+    int val;
+    scanf("%d",&val);
+    while(ptr->data != val || ptr->next!=NULL)
+    {
+        ptr = ptr->next;   
+    }
+    if(ptr==NULL)
+    {
+        printf("Entered value not found!!!\n");
+    }
+    else if(ptr->data == val){
+        
+    }
+}
+
+struct Node* delete(struct node *head){
+    printf("\n===========Deletion of a Node==========\n\n");
+    printf("How you want to delete a node \n");
+    printf("1. By value\n");
+    printf("2. By index\n");
+    printf("3. Main Menu\n");
+    printf("Enter your choice : ");
+    int ch;
+    scanf("%d", &ch);
+    switch (ch)
+    {
+    case 1: deleteByValue(head);
+            break;
+
+    case 2: deleteByIndex(head);
+            break;
+
+    case 3: exit;
+    
+    default: printf("Wrong choice entered!!!\n\n");
+            system("clear");
+            exit; 
+    }
+    return head;
+}
+
+//=============================================================================================
+
+
+
+
+int main(){
 
         struct Node *head;
-     while(1){
-        // printf("\t\t==============DS LAB Asignment================\n\n");
-        printf("===========Linked List==========\n\n");
-        printf("\t1. Create a new Linked List\n");
-        printf("\t2. Insert element in a Linked List\n");
-        printf("\t3. Delete element in a Linked List\n");
-        printf("\t4. Display the Linked list\n");
-        printf("\t5. Reverse a Linked List\n");
-        printf("\t6. Concatenate two Linked list\n");
-        printf("\t7. Circular Linked List\n");
-        printf("\t8. Exit\n");
-        printf("\t\tEnter the choice you want to perform : ");
+        head = NULL;
+        while(1){
+        displayLL(head);
+        printf("\n======================[Linked List]======================\n\n");
+        printf(" 1. Create a new Linked List\n");
+        printf(" 2. Insert element in a Linked List\n");
+        printf(" 3. Delete element in a Linked List\n");
+        printf(" 4. Reverse a Linked List\n");
+        printf(" 5. Concatenate two Linked list\n");
+        printf(" 6. Circular Linked List\n");
+        printf(" 7. Exit\n");
+        printf("Enter the choice you want to perform : ");
         int ch;
         scanf("%d",&ch);
 
         switch(ch)
         {
-            case 1:     head=(struct Node*)malloc(sizeof(struct Node));
-                        printf("Enter head elem : ");
-                        scanf("%d",&(head->data));
-                        head->next = NULL;
-                        displayLL(head);
-                        break;
-
+            case 1: head=(struct Node*)malloc(sizeof(struct Node));
+                    printf("Enter head elem : ");
+                    scanf("%d",&(head->data));
+                    head->next = NULL;
+                    break;
 
             case 2: head = insert(head);
                     break;
 
-            // case 3: delete();
-            //         break;
-
-            case 4: displayLL(head);
+            case 3: head = delete(head);
                     break;
 
-            // case 5: reverse();
+            // case 4: reverse();
             //         break;
 
-            // case 6: concatenate();
+            // case 5: concatenate();
             //         break;
 
-            // case 7: circularLL();
+            // case 6: circularLL();
             //         break;
 
-            case 8: exit;
+            case 7: return 0;
 
             default: printf("No choice choosen");
 
         }
     }
+    return 1;
 }
